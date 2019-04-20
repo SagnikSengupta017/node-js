7a76036af5b2de40587dff0ea3f1f1473e60279f
Sagnik Sengupta <sagnik.sengupta01@ad.infosys.com>
2019-04-20 23:00:23 +0530

    First node js server created

---
+++ First_node_js_Server.js
@@ -0,0 +1,16 @@
+//require function is used to include a module in node js code
+
+var http=require("http");
+
+//http is one of the built in module provided by node js used to create a http server
+
+//createServer is one of the function of http module which basically creates the server
+var server=http.createServer(function(request,response)
+{
+    //response is used to send response to the client
+response.writeHead(200,{"Content-Type":"text/html"});
+response.write("<html><body><h1 >Creation of node js asynchronous server</h1></body></html>");
+response.end();
+});
+server.listen(3011);//used to give the port number
+console.log("First node js server");

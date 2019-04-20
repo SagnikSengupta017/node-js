b90e4b1612d8b3049c3ce5cb37a15b15d291ef83
Sagnik Sengupta <sagnik.sengupta01@ad.infosys.com>
2019-04-21 00:34:41 +0530

    Node Web Server

--- First_node_js_Server.js
+++ First_node_js_Server.js
@@ -7,7 +7,7 @@ var http=require("http");
 //createServer is one of the function of http module which basically creates the server
 
 var module=require("./DBModule");
-var result1=module.authenticateUser("admin","admin");
+var result1=module.authenticateUser("admin","admin2");
 //here DBModule module is added to this js file
 //in this js file we are including the DBModule module,at runtime it will check for index.js file
 var server=http.createServer(function(request,response)
@@ -15,11 +15,9 @@ var server=http.createServer(function(request,response)
     //response is used to send response to the client
     var result=module.authenticateUser("admin","admin");
     response.writeHead(200,{"Content-Type":"text/html"});
-    response.write("<html><body><h1 >Hello ! Welcome to NODE JS</h1><br><br><h1>+'result'+</h1></body></html>");
-    
-    
+    response.write("<html><body><h1 >Hello ! Welcome to NODE JS</h1><br><br><h3>"+result+"</h3></body></html>");
     response.end();
 });
-server.listen(3011);//used to give the port number
+server.listen(3000);//used to give the port number
 console.log("node js server");
 console.log(result1);

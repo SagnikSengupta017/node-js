bbd5d876e9bdf45534d093d83788455872060896
Sagnik Sengupta <sagnik.sengupta01@ad.infosys.com>
2019-04-21 20:02:04 +0530

    NODE JS SERVER

--- First_node_js_Server.js
+++ First_node_js_Server.js
@@ -7,12 +7,14 @@ var http=require("http");
 //createServer is one of the function of http module which basically creates the server
 
 var module=require("./DBModule");
+//here as we are including DBModule , it will return an exports object which is being held by the reference variable module
 var result1=module.authenticateUser("admin","admin2");
 //here DBModule module is added to this js file
 //in this js file we are including the DBModule module,at runtime it will check for index.js file
 var server=http.createServer(function(request,response)
 {
     //response is used to send response to the client
+	//here authenticateUser function is invoked
     var result=module.authenticateUser("admin","admin");
     response.writeHead(200,{"Content-Type":"text/html"});
     response.write("<html><body><h1 >Hello ! Welcome to NODE JS</h1><br><br><h3>"+result+"</h3></body></html>");

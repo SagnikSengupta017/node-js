87f1f05d4834a18ed9b7814aa33e794c8a40a376
Sagnik Sengupta <sagnik.sengupta01@ad.infosys.com>
2019-04-21 20:03:56 +0530

    Creating one custom module

--- DBModule/index.js
+++ DBModule/index.js
@@ -1,9 +1,8 @@
-//exports ->instructions that tell node js which bits of code will be exported from this module to another module
+
 //here authenticateUser function of DBModule will be used in First_node_js_server.js file
 
-//In DBModule ,in index.js javascript file there is a function authenticateUser which will 
-//be used in different js files so used exports 
 
+//here we are adding each and every reusable methods to the exports object
 exports.authenticateUser=function(username,password)
 {
     if(username=="admin" && password=="admin")

085bc24e8775246b68e268f98fdca8541c5a7f9b
Sagnik Sengupta <sagnik.sengupta01@ad.infosys.com>
2019-04-23 00:23:17 +0530

    Node JS Server

--- Online_Book_Library/book_server.js
+++ Online_Book_Library/book_server.js
@@ -1,18 +1,28 @@
 var http=require("http");
-
-
 var module=require("./book");
+var url=require("url");// fetching  the buit-in url module
+var querystring=require("querystring") //fetching the querystring built-in module 
 
-var result1=module.authenticateUser("admin","admin2");
 
 var server=http.createServer(function(request,response)
 {
     
-     result=module.authenticateUser("admin","admin");
+    
+    query=url.parse(request.url).query;
+    //url.parse->parse the url, which means will divide the url into parts like protocol(http),path(localhost),port(3001),query string(data sent by the client like username and password)
+    //request.url->get the url, data passed by client to server having all the data
+    //url.parse(request.url).query->fetching the query string i.e data sent by client like username and password
+    username=querystring.parse(query)["UserName"];
+    //querystring.parse(query)->query variable stores the query string from the url
+    //parse method of querystring module will act on it and fetch the username and password
+    password=querystring.parse(query)["password"]; 
+    
+    result=module.authenticateUser(username,password);
+    
     response.writeHead(200,{"Content-Type":"text/html"});
     response.write("<html><body><h3>"+result+"</h3></body></html>");
     response.end();
 });
 server.listen(3001);
-console.log("node js server");
-console.log(result1);
+console.log("Server is running !!!");
+

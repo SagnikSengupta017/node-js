884ef7081b1724ddbec89ec2cf0f1bdce3405194
Sagnik Sengupta <sagnik.sengupta01@ad.infosys.com>
2019-05-15 21:57:15 +0530

    Function in JS Examples

--- D2_FirstClassFunctions/Starter/app.js
+++ D2_FirstClassFunctions/Starter/app.js
@@ -1,1 +1,14 @@
 // Your Javascript Code Goes Here
+//function statement
+function greet()
+{
+    console.log("Functions in JS");
+}
+greet();
+//functions in JS is first class
+
+function logGreet(fun)
+{
+    fun();
+}
+logGreet(greet);
